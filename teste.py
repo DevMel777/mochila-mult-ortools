72063bd6b24ba4781107f5302499ec971a21e0af
Melissa <programadorimd@hotmail.com>
2026-07-14 18:51:09 -0300

    adicionando variáveis de decisão

--- teste.py
+++ teste.py
@@ -7,7 +7,16 @@ def main():
     if not solver:
         print("Erro ao criar solver.")
         return
-
+ 
+    # Variáveis de decisão
+    # Valor item
+    valoir = [60, 100, 120]
+    # Peso item
+    peso = [10, 20, 30]
+    # Capacidade máxima da mochila
+    capacidade = 50
+   
+   
 
 if __name__ == "__main__":
     main()
