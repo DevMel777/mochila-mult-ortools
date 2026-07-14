b0d8479345b191e16dbac4a7e9acab17d9194b6c
Melissa <programadorimd@hotmail.com>
2026-07-14 19:31:07 -0300

    criando variável binária e exibindo variáveis no terminal

--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
+from ortools.init.python import init
 from ortools.linear_solver import pywraplp
 
 def main():
@@ -10,13 +11,21 @@ def main():
  
     # Variáveis de decisão
     # Valor item
-    valoir = [60, 100, 120]
+    valor = [60, 100, 120]
     # Peso item
     peso = [10, 20, 30]
     # Capacidade máxima da mochila
     capacidade = 50
-   
-   
+
+   # Criando variáveis de decisão binárias x0, x1, ..., xn
+   # Cada variável pode assumir apenas os valores 0 ou 1
+   # x_i ∈ {0,1}
+    x = []
+    for i in range(len(valor)):
+        # Se entra na mochila ou não
+        x.append(solver.IntVar(0, 1, f"x{i}"))  
+    
+    print(f"Variáveis criadas: {x, valor, peso, capacidade}") 
 
 if __name__ == "__main__":
     main()
